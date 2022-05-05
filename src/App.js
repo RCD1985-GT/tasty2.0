4382f4d02fd6f9a6b8b7e23c6479fa23e8e5d2fb
unknown <crazyspicefactory@gmail.com>
2022-05-05 15:02:16 +0200

    feat: registro actualiado

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from "./Containers/Home/Home";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
+import Registro from './Containers/Registro/Registro';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
 
         <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/registro" element={<Registro/>}/>
       
         </Routes>
 
