de320d239a539ef204cb4f7559223f743bcd35a0
unknown <crazyspicefactory@gmail.com>
2022-05-08 20:39:37 +0200

    Feat: types.jsx actualizado

--- src/redux/types.js
+++ src/redux/types.js
@@ -1,6 +1,15 @@
+// Guardar datos de usuario logueado
+export const LOGIN = 'login';
+export const LOGOUT = 'logout';
+export const MODIFY_CREDENTIALS = 'modify_credentials';
 
+// Guardar tipo de receta
 export const TIPO = 'tipo';
+
+// Guardar detalles de receta
 export const DETALLES_RECETA = 'detalles_receta';
-export const CREDENCIALES = 'credenciales';
+
+
+// export const CREDENCIALES = 'credenciales';
 
 
