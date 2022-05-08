e22a72369692befd8a9741d17e5a2081fbda2d22
unknown <crazyspicefactory@gmail.com>
2022-05-08 21:45:28 +0200

    feat: Home.jsx login y logout implementados y funcionando

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,25 +1,37 @@
 import React from 'react';
 import {useNavigate} from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import './Header.css';
+import { LOGOUT } from '../../redux/types';
+import { connect } from 'react-redux';
+import axios from 'axios';
 
 
 
-const Header = () => {
+const Header = (props) => {
 
 	let navigate = useNavigate();
 
 	const navegar = (lugar) => {
-
 		console.log("me meto aqui"); // mirar esto
-
         setTimeout(() => {
             navigate(lugar);
         }, 200);
+    }
 
+	const [nombre, setNombre] = useState("");
+
+	const logOut = () => {
+        //Borrar de RDX las credenciales
+        props.dispatch({ type: LOGOUT });
+
+        setTimeout(() => {
+            navigate("/");
+        }, 1500);
     }
 
 
-
+if (!props.credenciales?.token) {
 return(
 	
 	  <div className="contenedorHeader">
@@ -27,20 +39,45 @@ return(
 		<div className="logo" onClick={()=>navegar("/")}><b>Tasty</b></div>
 
        
-
 		<div className="entrar">
 			<div className="registro" onClick={()=>navegar("/register")}>Registro</div> 
 			
 			<div className="acceso" onClick={()=>navegar("/login")} >Acceso</div>
 
-			{/* <div className="admin" onClick={()=>navegar("/admin")} >Admin</div> */}
-			
+						
 		</div>
 
 		  
-
 	  </div>
-	);
-  };
+	)
+} else {
+	return ( 
 
-export default Header;
+		<div className="contenedorHeader">
+			<div className="logo" onClick={()=>navegar("/")}><b>Tasty</b></div>
+
+			
+		<div className="entrar">
+
+		<div className="link" onClick={()=>navegar('/perfil')} >{props.credenciales?.usuario.nombre} {props.credenciales?.usuario.apellido}</div>
+
+		<div className="" onClick={() => logOut()}>Logout</div>
+
+			{/* <div className="registro" onClick={()=>navegar("/register")}>Registro</div> 
+			
+			<div className="acceso" onClick={()=>navegar("/login")} >Acceso</div> */}
+		
+		</div>
+
+
+
+		</div>
+	)
+}
+};
+
+// export default Header;
+
+export default connect((state) => ({
+    credenciales: state.credenciales,
+  }))(Header);
