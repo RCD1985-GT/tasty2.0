45b24b1c99b0212a568adce6e56c4d5dc44b0de6
unknown <crazyspicefactory@gmail.com>
2022-05-13 19:31:40 +0200

    Perfil.jsx y Perfil.css actualizados

--- src/Containers/Perfil/Perfil.jsx
+++ src/Containers/Perfil/Perfil.jsx
@@ -30,7 +30,7 @@ const Perfil = (props) => {
     // Use effect 1
     useEffect(() => {
         if (props.credenciales.token === '') {
-           
+
         }
         mostrarRecetasGuardadas();
 
@@ -57,10 +57,10 @@ const Perfil = (props) => {
             headers: { Authorization: `Bearer ${props.credenciales.token}` }
         };
 
-        
+
         let res = await axios.get(`http://localhost:3300/guardados/usuario/${props.credenciales.usuario.id}`, config); // NO VA
         setRecetasGuardadas(res.data);
-        console.log("esto es resultao",res.data)
+        console.log("esto es resultao", res.data)
     }
 
     // // Funcion que actualiza usuario
@@ -97,33 +97,48 @@ const Perfil = (props) => {
     return (
         <div className="perfil">
 
-            <div className="datos">
-                <div className=''>
-                    <div className=" "><b>Nombre: </b>{props.credenciales.usuario.nombre}</div>
-                    <div className=" "><b>Apellidos: </b>{props.credenciales.usuario.apellido}</div>
-                    <div className=" "><b>Email: </b>{props.credenciales.usuario.email}</div>
-                    <div className=" "><b>Contraseña nueva:</b><input className='' type="text" name="contraseña" id="contraseña" title="contraseña" placeholder="Nueva Contraseña" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} /></div>
-                    {/* <div className="" onClick={() => updateUser()}>Update</div> */}
+            <div className="contenedorDatos">
+                <p>TUS DATOS PERSONALES:</p>
+                <div className="contenidoDatos">
+                    <div className="datos"><b>Nombre: </b>{props.credenciales.usuario.nombre}</div>
+                    <div className="datos"><b>Apellidos: </b>{props.credenciales.usuario.apellido}</div>
+                    <div className="datos"><b>Email: </b>{props.credenciales.usuario.email}</div>
+                    <div className="datos"><b>Contraseña nueva:</b><input className='' type="text" name="contraseña" id="contraseña" title="contraseña" placeholder="Nueva Contraseña" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} /></div>
+                    <div className="datosContraseña">Actualizar</div>
+                    {/* onClick={() => updateUser()} */}
                 </div>
             </div>
 
-            <div className="favoritas">
-                <div>
-                    <p>{props.credenciales.usuario.nombre},estas son tus recetas favoritas:</p>
+            <div className="contenedorFavoritas">
+                {/* <p>{props.credenciales.usuario.nombre},estas son tus recetas favoritas:</p> */}
+
+                <p>TUS RECETAS FAVORITAS</p>
+                <div className="contenidoFavoritas">
                     {
                         recetasGuardadas.map(receta => {
                             return (
-                                <div className="contenidoFavoritas" key={receta.id} >
-                                        Foto:  {receta.poster}<br/>
-                                        Titulo: {receta.titulo}<br/>
-                                        Tipo: {receta.tipo}<br/>
-                                        Ingredientes:{receta.ingredientes}<br/>
-                                        Preparacion:{receta.preparacion}
+                                <div className="cardReceta" key={receta.id} >
+
+                                    <div className="cardRecetaIzquierda">
+                                        <div className="cardRecetaIzquierdaFoto"> Foto:  {receta.poster}<br /></div>
+                                        <div className="cardRecetaIzquierdaTitulo"> Titulo: {receta.titulo}<br /></div>
+                                        {/* Titulo: {receta.titulo}<br />
+                                        Tipo: {receta.tipo}<br /> */}
+                                    </div>
+                                    
+                                    <div className="cardRecetaDerecha">
+                                        {/* Titulo: {receta.titulo}<br />
+                                        Tipo: {receta.tipo}<br /> */}
+                                        <div className="cardRecetaDerechaIngredientes"> Ingredientes:{receta.ingredientes}</div>
+                                        <div className="cardRecetaDerechaPreparacion">Preparacion:{receta.preparacion}</div>
+                                       
+                                    </div>
                                 </div>
                             )
                         })
                     }
                 </div>
+
             </div>
 
             <div>
