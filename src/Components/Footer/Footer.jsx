6d04817abe05922284edc15cfcdcb04fcdcc3479
unknown <crazyspicefactory@gmail.com>
2022-05-13 13:23:39 +0200

    feat: contacto añadido

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,31 +1,23 @@
 import React from 'react';
 import './Footer.css';
 
-
 const Footer = () => {
-	return(
-		
-	  <div className="contenedorFooter">
-		<div className="contenidoFooter">
-{/* 
-			<div className="arribaIzquierdaFooter">
-				<a href="#"> Contacto</a>
-				<a href="#"> Aviso Legal</a>
-			</div> */}
+	return (
 
-			<div className="iconosFooter">
-				<div className="iconoFacebook"></div> 
-				<div className="iconoInstagram"></div>
-				<div className="iconoTwitter"></div>
+		<div className="contenedorFooter">
+			<div className="contenidoFooter">
+				<div className="iconosFooter">
+					<div className="iconoFacebook"></div>
+					<div className="iconoInstagram"></div>
+					<div className="iconoTwitter"></div>
+				</div>
+			
+			
+			<div className="contactoFooter">
+				<a className='contacto' href="#"> Contacto</a>
 			</div>
+			</div>	
 		</div>
-
-		{/* <div className="abajoFooter">
-			  <div className="caja1"></div>
-			  <div className="caja2"></div>
-			  <div className="caja3"></div>
-		</div> */}
-	  </div>
 	);
-  };
+};
 export default Footer;
