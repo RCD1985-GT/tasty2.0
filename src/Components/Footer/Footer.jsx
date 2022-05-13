d0e480490648b54a5cd97833539485e541b809d1
unknown <crazyspicefactory@gmail.com>
2022-05-13 13:11:29 +0200

    feat: elementos contacto y aviso legal eliminados

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,25 +6,25 @@ const Footer = () => {
 	return(
 		
 	  <div className="contenedorFooter">
-		<div className="arribaFooter">
-
+		<div className="contenidoFooter">
+{/* 
 			<div className="arribaIzquierdaFooter">
 				<a href="#"> Contacto</a>
 				<a href="#"> Aviso Legal</a>
-			</div>
+			</div> */}
 
-			<div className="arribaDerechaFooter">
+			<div className="iconosFooter">
 				<div className="iconoFacebook"></div> 
 				<div className="iconoInstagram"></div>
 				<div className="iconoTwitter"></div>
 			</div>
 		</div>
 
-		<div className="abajoFooter">
+		{/* <div className="abajoFooter">
 			  <div className="caja1"></div>
 			  <div className="caja2"></div>
 			  <div className="caja3"></div>
-		</div>
+		</div> */}
 	  </div>
 	);
   };
